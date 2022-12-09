f36d343d912e652329f747b50958a6180e932655
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-09 15:59:33 -0300

    added messaging detailing with new Messanger class

--- src/classes/Atlas.py
+++ src/classes/Atlas.py
@@ -1,25 +1,16 @@
 from data.command_obj_list import commandManager
+from classes.Messanger import Messanger
+from utils.flatten import flatten
 import sys
 
 
-def flatten(l):
-    """transforms a list of lists in a single list 
-
-    Args:
-        l (list): a list object, populated with other lists
-
-    Returns:
-        list: returns a single list with separetted items
-    """
-    return [item for sublist in l for item in sublist]
-
-
 class Atlas:
-    """Atlas class to reference the atlas object that is used to run the commands 
+    """Atlas class to reference the atlas object that is used to run the commands
     and pluggins
     """
+
     def __init__(self, commands):
-        """construct a object
+        """construct a intance of Atlas class
 
         Args:
             commands (string): a string with the name of the command to be executed by the atlas object
@@ -27,7 +18,6 @@ class Atlas:
         self.commands = [_ for _ in commands[1:] if _[0] != "-"]
         self.flags = commands[2:]
         self.command = self.validate_arguments()
-        
 
     def run(self):
         """loops through all the commands from command_obj_list trying to find
@@ -39,44 +29,49 @@ class Atlas:
                 c.run(self.flags)
 
     def validate_arguments(self):
-        """method to validate the arguments passed to the atlas object, 
+        """method to validate the arguments passed to the atlas object,
         valid arguments are those containing a single command (and it's flags)
         this method tries to check if the command is valid by looping through the
         object list of command and all the valid flags inside all the objects
 
         Returns:
-            command: in case the command and flags are valid returns a stripped 
+            command: in case the command and flags are valid returns a stripped
             command to be used on the self.run() method
         """
-        # TODO: change validation by using the command_obj_list
         # check if the atlas keyword was called properly with valid arguments
         if len(self.commands) == 0:
             # if it was called with no arguments, prompt how to check the arguments
-            print("atlas <command> \ncheck commands using --atlas help--")
+            Messanger.message(
+                tag="warning",
+                text="atlas <command> check usage by typing --atlas help--",
+            )
+
             sys.exit()
         elif len(self.commands) > 1:
-            print("pass only one command at time")
+            Messanger.message(tag="failure", text="Pass only 1 command at time")
             sys.exit()
         else:
             # if it was called with invalid arguments, prompt to the user the invalid ones
             invalid_commands = [
-                _ for _ in self.commands if _ not in [i.name for i in commandManager.commands]
+                _
+                for _ in self.commands
+                if _ not in [i.name for i in commandManager.commands]
             ]
             if invalid_commands:
-                print(f"invalid command -- {' | '.join(invalid_commands)} --")
+                Messanger.message(
+                    tag="failure",
+                    text=f"invalid command -- {' | '.join(invalid_commands)} --",
+                )
                 sys.exit()
 
             invalid_flags = [
-                _ for _ in self.flags if _ not in flatten([_.flags for _ in commandManager.commands])
+                _
+                for _ in self.flags
+                if _ not in flatten([_.flags for _ in commandManager.commands])
             ]
 
-
             if invalid_flags:
-                print(
-                    f"invalid flags for command {self.commands[0]} found at -- {' | '.join(invalid_flags)} --"
-                )
-                print(
-                    f"check valid flags for {self.commands[0]} with -- atlas help -{self.commands[0]} --"
-                )
+                Messanger.message(tag="failure", text=f"invalid flags for command {self.commands[0]} found at -- {' | '.join(invalid_flags)} --")
+                Messanger.message(tag="hint", text=f"check valid flags for {self.commands[0]} with -- atlas help -{self.commands[0]} --")
                 sys.exit()
         return self.commands[0]
---
+++ src/classes/Messanger.py
@@ -0,0 +1,34 @@
+import sys
+from colorama import Fore
+
+class Messanger:
+
+    TAGS = ["sucess", "failure", "warning", "hint"]
+
+    @staticmethod
+    def message(text, tag):
+        """display a message in the terminal with a custom formatting
+            based on the tag given
+
+        Args:
+            text (_type_): text to be displayed
+            tag (_type_): type of message to display:
+                possible tags are: sucess | failure | warning
+        """
+        if tag not in [_ for _ in Messanger.TAGS]:
+            # if the tag is not available
+            print('MessagerTagError: invalid tag')
+            sys.exit()
+        
+        if tag == "sucess":
+            # checks if the tag represents a sucess message
+            print(Fore.GREEN + text)
+        elif tag == "failure":
+            # check if the tag represents a failure message
+            print(Fore.RED + f"ERROR!!!!\n{text}")
+        elif tag == "warning":
+            # check if the tag represents a warning message
+            print(Fore.YELLOW +  text)
+        elif tag == "hint":
+            # check if the tag represents a hinting message
+            print(Fore.CYAN + f"HINT\n{text}")
