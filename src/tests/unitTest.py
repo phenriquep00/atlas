2a874b0cedfa2716d3241910e2294c75891fff1c
Pedro Lima <contact.pedrolima@gmail.com>
2022-12-09 16:47:41 -0300

    setup unittest ambient

---
+++ src/tests/unitTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from classes.Atlas import Atlas
+
+class TestCommands(unittest.TestCase):
+    def setUp(self):
+        self.atlas = Atlas()
+
+
+if __name__ == '__main__':
+    unittest.main()
