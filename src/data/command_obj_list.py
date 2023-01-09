c7771c715536d4638ea5633581341fa41fe18e46
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-09 18:56:15 -0300

    added timer to command list

--- src/data/command_obj_list.py
+++ src/data/command_obj_list.py
@@ -2,12 +2,14 @@ from commands.Command import Test
 from commands.Help import Help
 from commands.PasswordGenerator import PasswordGenerator
 from commands.Vocab import Vocab
+from commands.Timer import Timer
 
 # objs
 test = Test(name="test", flags=Test.DOCS.keys())
 help = Help(name="help", flags=Help.DOCS.keys())
 pass_gen = PasswordGenerator(name="pass_gen", flags=PasswordGenerator.DOCS.keys())
 vocab = Vocab(name="vocab", flags=Vocab.DOCS.keys())
+timer = Timer(name="timer", flags=Timer.DOCS.keys())
 
 command_obj_list = [
     test,
