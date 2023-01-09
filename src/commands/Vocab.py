332ee0b0f3d8c70721ed1cc1b946d51210d526d6
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-09 17:38:29 -0300

    documentarion update on vocab command

--- src/commands/Vocab.py
+++ src/commands/Vocab.py
@@ -5,15 +5,33 @@ import requests
 
 
 class Vocab(Command):
+    """Display data about a single word from the user input
 
+    Args:
+        Command (Commans): Command class
+    Returns:
+        _type_: none
+    """
+
+    # command documentation
     DOCS = {
         "-help": "Vocabulary command (english only for now)",
     }
 
+    # initializing the command
     def __init__(self, name, flags):
+        # inherit attributes
         super().__init__(name, flags)
 
     def run(self, options):
+        """runs the vocab command
+        validating the flags and the asking the user for the word do analyse
+        the method then proceeds to find the word in the api. If the word exists
+        in the api, returns information about it
+
+        Args:
+            options (_type_): command options
+        """
         self.validate(options=options)
         self.help(docs=Vocab.DOCS, options=options)
 
@@ -49,6 +67,11 @@ class Vocab(Command):
 
     @staticmethod
     def check_word():
+        """checks if the word contains only ascii letters characters
+
+        Returns:
+            _word: returns the word in case it is valid
+        """
         word = str(input("What word would you like to get information from: \n"))
         for letter in word:
             if letter not in string.ascii_letters:
@@ -58,6 +81,11 @@ class Vocab(Command):
 
     @staticmethod
     def formatted_data(data):
+        """display the formatted information from the api about the word
+
+        Args:
+            data (obj): the response from the api
+        """
         Messanger.message(
             tag="success", text=f"Prompting data for the word {data['word'].upper()}"
         )
