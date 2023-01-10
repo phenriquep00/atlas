c82612d9e04efb2574973642ff8fbe7576ca5b79
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-10 18:28:49 -0300

    implemented timer countdown functionality

--- src/commands/Timer.py
+++ src/commands/Timer.py
@@ -1,10 +1,12 @@
 from classes.Messanger import Messanger
 from commands.Command import Command
+from time import sleep
 
 
 class Timer(Command):
     DOCS = {}
-    def __init__(self, name:str, flags):
+
+    def __init__(self, name: str, flags):
         super().__init__(name, flags)
 
     def run(self, options):
@@ -12,3 +14,23 @@ class Timer(Command):
         self.help(docs=Timer.DOCS, options=options)
 
         # command execution goes here
+
+        # aks for the user the amount os time (in seconds) to countdown
+        while ...:
+            try:
+                t = int(input("enter the time to countdown (in seconds): \n"))
+                break
+            except ValueError:
+                Messanger.message(
+                    tag="failure",
+                    text="invalid! use only integer numbers. try again or exit [ctrl + c]",
+                )
+
+        while t:
+            mins, secs = divmod(t, 60)
+            timer = f"{mins:02d}:{secs:02d}"
+            print(timer, end="\r")
+            sleep(1)
+            t -= 1
+
+        Messanger.message(tag="success", text="the countdown has succesfully finished")
