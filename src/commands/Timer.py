158f45d68d8cf4a462b0bb0759e02e73437de501
Pedro Lima <contact.pedrolima@gmail.com>
2023-01-10 18:34:22 -0300

    added documentation to timer command countdown functionality

--- src/commands/Timer.py
+++ src/commands/Timer.py
@@ -4,13 +4,35 @@ from time import sleep
 
 
 class Timer(Command):
-    DOCS = {}
+    """ Timer class to implement the timer command used by atlas
+    this command has the responsability to implement time related functions
 
-    def __init__(self, name: str, flags):
+    Args:
+        Command (Command): super class
+    """
+    DOCS = {
+        "-help": "the timer command can be used for opperations with time, right now, the only usability available is the countdown function"
+    }
+
+    def __init__(self, name: str, flags: list):
+        """class constructor
+
+        Args:
+            name (str): name of the command to be called with atlas 
+            flags (list): list of possible options to be used with this command
+        """
         super().__init__(name, flags)
 
     def run(self, options):
+        """
+        run the implementation of this command 
+
+        Args:
+            options (_type_): given options when the command was called with atlas
+        """
+        # validade the flags comparing them with the available options
         self.validate(options=options)
+        # in case the -help option was called, runs this method and displays the ocmmand usability
         self.help(docs=Timer.DOCS, options=options)
 
         # command execution goes here
@@ -26,6 +48,7 @@ class Timer(Command):
                     text="invalid! use only integer numbers. try again or exit [ctrl + c]",
                 )
 
+        # start the countdown loop
         while t:
             mins, secs = divmod(t, 60)
             timer = f"{mins:02d}:{secs:02d}"
@@ -33,4 +56,5 @@ class Timer(Command):
             sleep(1)
             t -= 1
 
+        # display that the command has been completed without errors
         Messanger.message(tag="success", text="the countdown has succesfully finished")
