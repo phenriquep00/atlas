6ef85293830993be4d9d2c731f89d69ce866d686
Pedro Henrique <contact.pedrolima@gmail.com>
2023-03-08 10:40:59 -0300

    Update atlas.py

--- src/atlas.py
+++ src/atlas.py
@@ -1,10 +1,12 @@
 import os
 import click
 
+
 class Atlas(object):
     def __init__(self, home=None, debug=False):
-        self.home = os.path.abspath(home or'.')
+        self.home = os.path.abspath(home or '.')
         self.debug = debug
+
 
 @click.group()
 @click.option('--atlas-home', envvar='ATLAS_HOME', default='.atlas')
@@ -13,7 +15,9 @@ class Atlas(object):
 def cli(ctx, atlas_home, debug):
     ctx.obj = Atlas(atlas_home, debug)
 
+
 pass_atlas = click.make_pass_decorator(Atlas, ensure=True)
+
 
 @cli.command()
 @click.argument('src')
@@ -21,7 +25,8 @@ pass_atlas = click.make_pass_decorator(Atlas, ensure=True)
 @pass_atlas
 def clone(repo, src, dest):
     click.echo(src)
-    
+
+
 @cli.command()
 @pass_atlas
 def cp(repo):
