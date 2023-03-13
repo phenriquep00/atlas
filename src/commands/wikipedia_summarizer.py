8921592c7717af23729413eada196269be822408
Pedro Henrique <contact.pedrolima@gmail.com>
2023-03-13 11:04:35 -0300

    added speech summary to wikipedia_summarizer command

--- src/commands/wikipedia_summarizer.py
+++ src/commands/wikipedia_summarizer.py
@@ -1,7 +1,11 @@
-import click
 from commands.command import Command
 
+import os
+import click
 import wikipedia
+import playsound
+
+from gtts import gTTS
 
 
 class WikipediaSummarizer(Command):
@@ -12,6 +16,20 @@ class WikipediaSummarizer(Command):
         @self.cli.command(help="get information from a given topic based on it's wikipedia article")
         @click.option("--article", required=True, help="the name of the article to be summarized")
         @click.option("--len", default=2, type=int, help="lenght of the summary to be printed (number of sentences)", show_default=True)
-        def wiki(article, len):
+        @click.option("--read", default=False, is_flag=True, help="read the summary")
+        @click.option("--hide", default=False, is_flag=True, help="hide the textual summary")
+        def wiki(article, len, read, hide):
             summary = wikipedia.summary(article, sentences=len)
-            click.echo(summary)
+            if not hide:
+                click.echo(summary)
+            if read:
+                WikipediaSummarizer.speak(summary)
+
+    @staticmethod
+    def speak(text):
+        tts = gTTS(text=text, lang='en')
+
+        filename = "abc.mp3"
+        tts.save(filename)
+        playsound.playsound(filename)
+        os.remove(filename)
